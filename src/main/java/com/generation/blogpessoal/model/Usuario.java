db35a13a73c90447921c786b10a0ba861a55b8c3
João Vitor Felix de Andrade <joao.felixandrade1201@gmail.com>
2022-05-16 12:54:39 -0300

    Realizando configurações para o Deploy

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -16,6 +16,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name = "tb_usuarios")
 public class Usuario {
@@ -27,8 +29,9 @@ public class Usuario {
 	@NotNull(message = "o atributo nome é obrigatório")
 	private String nome;
 	
-	@NotNull
-	@Email(message = "O atributo usuário precisa ser um e-mail válido")
+	@Schema(example = "email@email.com.br")
+	@NotNull(message = "O atributo Usuário é Obrigatório!")
+	@Email(message = "O atributo Usuário precisa ser um e-mail válido")
 	private String usuario;
 	
 	@NotBlank
